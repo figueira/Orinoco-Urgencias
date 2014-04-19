ba17ee935bce3acaf92bb3bab4f93fc4da815ff4
ivantrave <ivantrave@gmail.com>
2014-04-18 21:54:12 -0430

    Arreglando interfaz

--- static/scripts/esperas.js
+++ static/scripts/esperas.js
@@ -1,4 +1,4 @@
-/* 
+  /* 
   Hace que una serie de causas de espera se mantengan activas
   Entrada:
     El objeto de jQuery correspondiente a la fila en la que la espera se 
@@ -9,5 +9,8 @@ function MantenerEsperas(fila_emergencia){
   fila_emergencia.data("tiempoesp", 0);
   fila_emergencia.data("tiempo_espera_inicio","0");
   fila_emergencia.css("background","white");
+
+  //var circulo = document.getElementById('circulo_'+id_emergencia);
+  //circulo.removeAttribute('src');
   $.get("/emergencia/espera_mantener/" + id_emergencia);
 }
