16ab8343fb062751a83fcebc0c5ace88b888a84c
ivantrave <ivantrave@gmail.com>
2014-06-18 08:59:52 -0430

    Archivo de migracion para linux

---
+++ BD/migracion/migracion.sh
@@ -0,0 +1,14 @@
+python edicionTablas.py
+python ../../manage.py dumpdata auth.user app_usuario.usuario app_paciente.paciente app_emergencia.emergencia app_emergencia.triage app_emergencia.atencion app_emergencia.esperaemergencia > BDbackup.json 
+python eliminacionTablas.py
+python ../../manage.py syncdb
+python ../../manage.py loaddata ../aadmision.json
+python ../../manage.py loaddata ../aemergencia.json
+python ../../manage.py loaddata ../at_indicaciones.json
+python ../../manage.py loaddata ../atencion_cuerpo_sintomas.json
+python ../../manage.py loaddata ../cubiculo.json
+python ../../manage.py loaddata ../edestino.json
+python ../../manage.py loaddata ../espera.json
+python ../../manage.py loaddata ../motivos.json
+python loadEnfermedades.py
+python ../../manage.py loaddata BDbackup.json
