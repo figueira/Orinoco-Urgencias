f28210ab803b2cee8bd81a58cc61054285fa18ea
leofaster <leofaster@aniquilator>
2014-11-06 01:19:05 -0500

    Colocando bien las dependencias de las migraciones

--- app_emergencia/migrations/0001_initial.py
+++ app_emergencia/migrations/0001_initial.py
@@ -6,6 +6,10 @@ from django.db import models
 
 
 class Migration(SchemaMigration):
+    depends_on = (
+        ("app_paciente", "0001_initial"),
+
+    )
 
     def forwards(self, orm):
         # Adding model 'AreaEmergencia'
